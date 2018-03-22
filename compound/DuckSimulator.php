12898716f36e3c563229b44cc697f42e52b3b84c
shiw111 <shiw>
2018-03-22 17:42:10 +0900

    컴파운드 패턴 - 2. 어댑터 패턴

--- compound/DuckSimulator.php
+++ compound/DuckSimulator.php
@@ -3,6 +3,8 @@ require_once 'MallardDuck.php';
 require_once 'RedheadDuck.php';
 require_once 'DuckCall.php';
 require_once 'RubberDuck.php';
+require_once 'Goose.php';
+require_once 'adapter/GooseAdapter.php';
 
 class DuckSimulator
 {
@@ -12,12 +14,14 @@ class DuckSimulator
         $redheadDuck = new RedheadDuck();
         $duckCall = new DuckCall();
         $rubberDuck = new RubberDuck();
+        $goose = new GooseAdapter(new Goose());
 
         echo "<br>Duck Simulator<br>";
         $this->simulator($mallardDuck);
         $this->simulator($redheadDuck);
         $this->simulator($duckCall);
         $this->simulator($rubberDuck);
+        $this->simulator($goose);
     }
 
     /** @var Quackable $duck */
